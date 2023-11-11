2ac48964521b44beb9522611f18aa3874c16d2e6
hoang.danh.quan <hoangdanhquan48@gmail.com>
2023-11-11 10:48:03 +0700

    useEffect hook

--- src/Content.js
+++ src/Content.js
@@ -1,6 +1,31 @@
+import { useState, useEffect } from "react"
+
 function Content(){
+    const [title, setTitle] = useState('')
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(res => res.json())
+            .then(posts => {
+                setPosts(posts)
+            })
+    }, [])
+
+
     return (
-        <h1>Hi anh em</h1>
+        <div>
+            <input
+                value={title}
+                onChange={e => setTitle(e.target.value)}    
+            />
+            <ul>
+                {posts.map(post => (
+                    <li key={post.id}>{post.title}</li>
+                ))}
+            </ul>
+
+        </div>
     )
 }
 
