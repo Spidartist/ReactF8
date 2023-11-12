8b4c2748fad927838c059c8e07e4bb013814cb04
hoang.danh.quan <hoangdanhquan48@gmail.com>
2023-11-12 22:28:17 +0700

    useEffect 4

--- src/Content.js
+++ src/Content.js
@@ -3,69 +3,20 @@ import { useState, useEffect } from "react"
 const tabs = ["posts", "comments", "albums"]
 
 function Content(){
-    const [title, setTitle] = useState('')
-    const [posts, setPosts] = useState([])
-    const [type, setType] = useState("posts")
-    const [showGoToTop, setShowGoToTop] = useState(false)
-
+    const [countdown, setCountdown] = useState(180)
 
     useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/${type}`)
-            .then(res => res.json())
-            .then(posts => {
-                setPosts(posts)
-            })
-    }, [type])
+        const timeID = setInterval(() => {
+            setCountdown(prev => prev - 1)
+            console.log("Countdown...")
+        }, 1000);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            setShowGoToTop(window.scrollY >= 200)
-        }
-
-        window.addEventListener('scroll', handleScroll)
-
-        // Clean up function
-        return () => {
-            window.removeEventListener('scroll', handleScroll)
-        }
+        return () => clearInterval(timeID)
     }, [])
-
 
     return (
         <div>
-            {tabs.map(tab => (
-                <button 
-                    key={tab}
-                    style={type === tab ? {
-                        color: '#fff',
-                        backgroundColor: '#333'
-                    } : {}}
-                    onClick={() => setType(tab)}
-                >
-                    {tab}
-                </button>
-            ))}
-
-            <input
-                value={title}
-                onChange={e => setTitle(e.target.value)}    
-            />
-            <ul>
-                {posts.map(post => (
-                    <li key={post.id}>{post.title || post.name}</li>
-                ))}
-            </ul>
-            {showGoToTop && (
-                <button
-                    style={{
-                        position: "fixed",
-                        right: 20,
-                        bottom: 20
-                    }}
-                >
-                    Go to Top
-                </button>
-            )}
+            <h1>{countdown}</h1>
         </div>
     )
 }
