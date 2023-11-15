b63ac59b8e194259dbe014539f1f61b215fa24b7
hoang.danh.quan <hoangdanhquan48@gmail.com>
2023-11-15 17:34:40 +0700

    useContext

--- src/Content.js
+++ src/Content.js
@@ -1,38 +1,10 @@
-import { useState, useMemo } from "react"
+import Paragraph from "./Paragraph"
 
 
 function Content(){
-    const [name, setName] = useState('')
-    const [price, setPrice] = useState('')
-    const [products, setProducts] = useState([])
-
-    const handleSubmit = () => {
-
-    }
-
     return (
-        <div style={{padding: '10px 32px'}}>
-                <input
-                    value={name}
-                    placeholder="Enter name..."
-                    onChange={e => setName(e.target.value)}
-                />
-                <br/>
-                <input
-                    value={price}
-                    placeholder="Enter price..."
-                    onChange={e => setPrice(e.target.value)}
-                />
-                <br/>
-                <button onClick={handleSubmit}>Add</button>
-                <br/>
-                Total:
-                <ul>
-                    {products.map((product, index) => (
-                        <li key={index}>{product.name}-{product.price}</li>
-                    ))}
-
-                </ul>
+        <div>
+            <Paragraph />
         </div>
     )
 }
---
+++ src/Paragraph.js
@@ -0,0 +1,15 @@
+import { ThemeContext } from "./ThemeContext"
+import { useContext } from "react"
+
+function Paragraph(){
+
+    const context = useContext(ThemeContext)
+
+    return (
+        <p className={context.theme}>
+            Context provides a way to pass data through the component tree without having to pass props down manually at every level.
+        </p>
+    )
+}
+
+export default Paragraph
